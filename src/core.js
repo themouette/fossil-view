11f289e7929a78f70ab813ac3c88b3163bf5f080
Julien Muetton <themouette@gmail.com>
2013-08-13 16:09:04 +0200

    Use Fossil.View.render method to render.

--- src/core.js
+++ src/core.js
@@ -1,10 +1,15 @@
 // This file defines the Fossil View base component.
 // It is required for any of the Fossil view component.
-var Lib = (function (Fossil, Backbone) {
+var Lib = (function (Fossil, _, Backbone) {
     "use strict";
 
-    Fossil.View || (Fossil.View = Backbone.View.extend({}));
+    Fossil.View || (Fossil.View = Backbone.View.extend({
+                        render: function () {
+                            this.$el.html(_.result(this, "template"));
+                            return this;
+                        }
+                    }));
     Fossil.Views || (Fossil.Views = {});
 
     return Fossil.Views;
-})(Fossil, Backbone);
+})(Fossil, _, Backbone);
