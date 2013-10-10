58dfacda5bba30a047f9f3f4e5770d8d69cc7ab7
Julien Muetton <themouette@gmail.com>
2013-10-10 23:28:14 +0200

    Add non AMD version templates.

---
+++ build/templates/footer.js
@@ -0,0 +1,12 @@
+    var view = require('view');
+    var composite = require('composite');
+    var collection = require('collection');
+    var regionManager = require('regionManager');
+
+    return {
+        "View": require('view'),
+        "Composite": require('composite'),
+        "Collection": require('collection'),
+        "RegionManager": require('regionManager')
+    };
+})(_, Backbone);
